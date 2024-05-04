3d49941ec7b147dab7147458120f31edfde1f045
Amaimoncz <amaimoncz@seznam.cz>
2024-05-04 22:33:37 +0200

    update 4.0.3

--- v21_omega/skin.estuary.amai/script/startupprogressbar.py
+++ v21_omega/skin.estuary.amai/script/startupprogressbar.py
@@ -1,17 +1,23 @@
 import xbmc
 import xbmcgui
 import time
+import sys
+
 
 dialog = xbmcgui.DialogProgressBG()
 dialog.create("Kodi", "Loading widgets & accesories ...")
 
+# set delay time from argument
+delayTime = int(sys.argv[1])
+
+# progres bar function
 def update_progress_bar(percent):
     dialog.update(percent, "Starting Kodi...    Please Wait ...")
 
 def main():
     for i in range(0, 101):
         update_progress_bar(i)
-        time.sleep(0.20)  # Simulace čekání na načítání
+        time.sleep(delayTime / 100)  # delay time for start kodi
 
     dialog.close()
 
